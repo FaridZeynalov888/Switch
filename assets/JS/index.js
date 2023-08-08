088757152fe2de652b7e95843fa7510581961bb2
FaridZeynalov888 <farid.zeynalovc96@gmail.com>
2023-08-09 00:06:27 +0400

    init first commit

---
+++ assets/JS/index.js
@@ -0,0 +1,54 @@
+// ! Task 1
+let a = prompt("Enter the month:")
+
+switch(a){
+    case "January":
+        alert("First month - Winter seasons")
+        break;
+    case "February":
+        alert("Second month - Winter seasons")
+        break;
+    case "March":
+        alert("Third month - Spring seasons")
+        break
+    case "April":
+        alert("Fourth month - Spring seasons")
+        break;
+    case "May":
+        alert("Fifth month - Spring seasons")
+        break;
+    case "June":
+        alert("Sixth month - Summer seasons")
+        break;
+    case "July":
+        alert("Seventh month - Summer seasons")
+        break;
+    case "August":
+        alert("Eighth month - Summer seasons")
+        break;
+    case "September":
+        alert("Ninth month - Autumn seasons")
+        break;
+    case "October":
+        alert("Tenth month - Autumn seasons")
+        break;
+    case "November":
+        alert("Eleventh month - Autumn seasons")
+        break;
+    case "December":
+        alert("Twelfth month - Winter seasons")
+        break;
+        default:
+            alert("There is no such month");
+}
+
+// ! Task 2
+// let a = "salam";
+// let b = "k"
+
+// for(let i = 0; i < a.length ; i++){
+//     if(a[2] == 'l'){
+//         console.log(a.replace(a[2],b));
+//     }
+// }
+
